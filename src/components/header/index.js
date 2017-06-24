e7ee008b12e0b850d6664a3bb36163ad8514098e
Felipe Torres (fforres) <felipe.torressepulveda@gmail.com>
2017-06-23 23:22:06 -0400

    Adding 'Responsivenes'

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,17 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
+import Icon from '../../assets/noders_icons/noders_white.svg';
 
 export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<nav>
-					<Link activeClassName={style.active} href="/">Eventos</Link>
+				<nav class={style.nav}>
+					<Link activeClassName={style.active} href="/">
+						<img src={Icon} class={style.logoIcon} />
+					</Link>
+					<Link activeClassName={style.active} href="/Eventos">Eventos</Link>
 					<Link activeClassName={style.active} href="/profile">Media</Link>
 					<Link activeClassName={style.active} href="/profile/john">Comunidad</Link>
 				</nav>
