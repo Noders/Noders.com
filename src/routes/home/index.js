5c351d407675f4755385197b523075dad39a0c3c
Fernando Larrañaga <xabadu@gmail.com>
2017-06-24 20:35:11 -0700

    Add events section

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,14 @@
 import { h, Component } from 'preact';
 import style from './style';
 import Hero from '../../components/hero';
+import Events from '../../components/events';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
 				<Hero />
+        <Events />
 			</div>
 		);
 	}
