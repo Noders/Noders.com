0a575f92c51159a426e13007f9f3ede83ee994a7
Fernando Larrañaga <xabadu@gmail.com>
2017-06-25 19:37:45 -0700

    Add events section (#13)

---
+++ src/components/events/index.js
@@ -0,0 +1,38 @@
+import { h } from 'preact';
+import { Link } from 'preact-router';
+import style from './style';
+
+const Events = (props, context) =>
+  (
+    <section className={style.eventsSection}>
+      <div className={style.eventsTitle}>
+        <h1 className="title">Eventos</h1>
+        <hr />
+      </div>
+      <div className="level">
+        <div className="level-item has-text-centered">
+          <figure className={style.eventImage}>
+            <Link href="/events/santiagojs">
+              <img src="../../assets/images/meetupsjs.png" alt="santiagojs" />
+            </Link>
+          </figure>
+        </div>
+        <div className="level-item has-text-centered">
+          <figure className={[style.eventImage, style.centerContent]}>
+            <Link href="/events/nodeschool">
+              <img src="../../assets/images/nodeschool.png" alt="nodeschoolsantiago" />
+            </Link>
+          </figure>
+        </div>
+        <div className="level-item has-text-centered">
+          <figure className={style.eventImage}>
+            <Link href="/events/eventloop">
+              <img src="../../assets/images/eventloop.png" alt="eventloop" className={style.eventLoopLogo} />
+            </Link>
+          </figure>
+        </div>
+      </div>
+    </section>
+  );
+
+export default Events;
