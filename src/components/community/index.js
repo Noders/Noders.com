a4e88967a64057ab008d3233f7ec77777d2e5fc4
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2017-06-29 09:23:58 -0700

    Adding eslint config and fixing files through prettier (#18)

--- src/components/community/index.js
+++ src/components/community/index.js
@@ -1,26 +1,24 @@
 import { h } from 'preact';
-import { Link } from 'preact-router';
 import style from './style';
 import members from '../../assets/data/members';
 
-const Community = (props, context) =>
-  (
-    <section className="section-container">
-      <div className="section-title">
-        <h1 className="title">Comunidad</h1>
-        <hr />
-      </div>
-      <div className="level">
+const Community = () => (
+  <section className="section-container">
+    <div className="section-title">
+      <h1 className="title">Comunidad</h1>
+      <hr />
+    </div>
+    <div className="level">
       {members.map((member) =>
         (<div className="level-item has-text-centered">
           <figure>
-          <a href={`https://twitter.com/${member.twitter}`}><img src={`${member.image}`} className={style.memberImage} /></a>
-          <p className={style.memberInfo}><a href={`https://twitter.com/${member.twitter}`}>@{member.username}</a></p>
-        </figure>
+            <a href={`https://twitter.com/${member.twitter}`}><img src={`${member.image}`} className={style.memberImage} /></a>
+            <p className={style.memberInfo}><a href={`https://twitter.com/${member.twitter}`}>@{member.username}</a></p>
+          </figure>
         </div>)
       )}
-      </div>
-    </section>
-  );
+    </div>
+  </section>
+);
 
 export default Community;
--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -3,19 +3,19 @@ import style from './style';
 import image from '../../assets/noders/noderslogo_2.png';
 
 export default class Header extends Component {
-	componentDidMount() {
-		setTimeout(() => this.setState({ load: true }), 750);
-	}
-	render() {
-		const showable = this.state.load ? style.showable : null ;
-		return (
-			<div class={style.fullHeight}>
-				<div class={[style.fullHeight, style.background].join(' ')} />
-				<div class={[style.fullHeight, style.content, showable].join(' ')}>
-					<img src={image}  />
-					<h1 >¿Por qué? Porque nos gusta</h1>
-				</div>
-			</div>
-		);
-	}
+  componentDidMount() {
+    setTimeout(() => this.setState({ load: true }), 750);
+  }
+  render() {
+    const showable = this.state.load ? style.showable : null;
+    return (
+      <div class={style.fullHeight}>
+        <div class={[style.fullHeight, style.background].join(' ')} />
+        <div class={[style.fullHeight, style.content, showable].join(' ')}>
+          <img src={image} />
+          <h1 >¿Por qué? Porque nos gusta</h1>
+        </div>
+      </div>
+    );
+  }
 }
