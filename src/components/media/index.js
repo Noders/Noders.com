4e2b6dbba43cc8bd9aef818fb5886d33170245e5
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-06-29 09:48:08 -0700

    adding links to media

--- src/components/media/index.js
+++ src/components/media/index.js
@@ -1,5 +1,4 @@
 import { h } from 'preact';
-import { Link } from 'preact-router/match';
 import style from './style';
 import nodersYoutube from '../../assets/images/noders-youtube-200.png';
 import nodersCast from '../../assets/images/noders-cast-200.png';
@@ -13,18 +12,18 @@ export default () => (
     <div className="level">
       <div className="level-item has-text-centered">
         <figure>
-          <Link href="/media/youtube" className={style}>
+          <a href="https://www.youtube.com/channel/UC7tUsO3S7424TMcgSCUOCow/feed" target="_blank" className={style}>
             <img src={nodersYoutube} class={style.image} alt="youtube" />
             <h3 class={style.imageTitle}> Youtube </h3>
-          </Link>
+          </a>
         </figure>
       </div>
       <div className="level-item has-text-centered">
         <figure>
-          <Link href="/media/noderscast" className={style.imageBackground}>
+          <a href="https://www.youtube.com/playlist?list=PLBEwfn9JYDc-8LKrnlcogwrILF08EqLHF" target="_blank" className={style}>
             <img src={nodersCast} class={style.image} alt="noderscast" credits="podcast by Nate Eul from the Noun Project" />
             <h3 class={style.imageTitle}> NodersCast </h3>
-          </Link>
+          </a>
         </figure>
       </div>
     </div>
