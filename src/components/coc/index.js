44b77cbe630f5daaf39157e1e7ef39e3b386ee75
Fernando Larrañaga <xabadu@gmail.com>
2017-07-03 13:40:34 -0700

    Add Code of Conduct section

---
+++ src/components/coc/index.js
@@ -0,0 +1,25 @@
+import { h } from 'preact';
+import style from './style';
+import { classJoin } from '../../helpers';
+
+const CodeOfConduct = () => (
+  <section className="section-container">
+    <div className="section-title">
+      <h1 className="title">Código de Conducta</h1>
+      <hr />
+    </div>
+    <div className="container">
+      <div className={classJoin(style.cocText, 'content', 'has-text-centered')}>
+        <i class={classJoin('fa fa-file-text-o', style.cocIcon)} />
+        <p>
+          Todos nuestros eventos se rigen y respetan este <a href="https://github.com/Noders/code_of_conduct" target="_blank">Código de Conducta</a>.
+        </p>
+        <p>
+          Si quieres participar de alguno de ellos, te pedimos que lo respetes y que nos ayuden a que otros lo hagan.
+        </p>
+      </div>
+    </div>
+  </section>
+);
+
+export default CodeOfConduct;
