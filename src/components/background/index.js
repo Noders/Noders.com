67ccedf35fff67aef2890725776bda5ea4c9e9d7
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-07-22 12:14:36 -0700

    cleanup

--- src/components/background/index.js
+++ src/components/background/index.js
@@ -1,7 +1,5 @@
 import { h, Component } from 'preact';
-import { Link } from 'preact-router/match';
 import style from './style';
-import Icon from '../../assets/noders_icons/noders_white.svg';
 import { classJoin } from '../../helpers';
 
 export default class Background extends Component {
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,10 @@
 import { h, Component } from 'preact';
 import style from './style';
-import Background from '../../components/background';
 import Hero from '../../components/hero';
 import Events from '../../components/events';
 import Media from '../../components/media';
 import Organizers from '../../components/organizers';
 import Noders from '../../components/noders';
-import Community from '../../components/community';
 import CodeOfConduct from '../../components/coc';
 import { classJoin } from '../../helpers';
 
