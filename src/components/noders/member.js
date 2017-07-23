b1014c01e2f90b7d81987001ad6b339521c67626
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-07-22 20:40:25 -0700

    Adding fix with SVG for hexagons

--- src/components/noders/member.js
+++ src/components/noders/member.js
@@ -3,7 +3,7 @@ import style from './style';
 import { classJoin } from '../../helpers';
 
 export default class Member extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       active: false
@@ -13,25 +13,29 @@ export default class Member extends Component {
   clicked = () => {
     this.setState({
       active: !this.state.active
-    })
-  }
-  
+    });
+  };
+
   render() {
     const { username, image } = this.props;
     const activeClass = this.state.active ? style.activeHexagon : null;
     return (
-      <div class={style.hexagonContainer}>
-        <div class={classJoin(style.hexagon, style.hexagon2, activeClass)} onClick={this.clicked}>
-          <div class={style['hexagon-in1']}>
-            <div class={style['hexagon-in2']} style={{
-              backgroundImage: `url(${image})`
-            }} />
-            <div class={style['hexagon-in-layout']}>
-              <span> { username } </span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <svg
+        viewBox="0 0 100 100"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          height: '100px'
+        }}
+      >
+        <defs>
+          <pattern id={image} patternUnits="userSpaceOnUse" width="100" height="100">
+            <image href={image} x="-25" width="150" height="100" />
+          </pattern>
+        </defs>
+        <polygon points="50 1 95 25 95 75 50 99 5 75 5 25" fill={`url(#${image})`}/>
+      </svg>
+
     );
   }
 }
