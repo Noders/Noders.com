2d401694c67c7ac1e7714616034542505152687b
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-07-22 21:38:46 -0700

    - !

--- src/components/noders/index.js
+++ src/components/noders/index.js
@@ -32,7 +32,7 @@ export default class Noders extends Component {
         <div className="container">
           <a name="comunidad" />
           <div className="section-title">
-            <h1 className="title">Comunidad! </h1>
+            <h1 className="title">Comunidad</h1>
             <hr />
           </div>
           <div class={classJoin('level', style.flexContainer, 'section-content')}>
