3f0d33d61be2d8f63804b4aa5e54751d85b3fdd4
Ramón Chancay Ortega <ramon.chancay@gmail.com>
2017-09-18 15:26:35 -0500

    Fixing double scroll #67. various styles.  Changes to be committed: 	new file:   .yarnrc 	modified:   src/components/forkMeRibbon/index.js 	modified:   src/components/forkMeRibbon/style.less 	modified:   src/components/noders/data.json 	modified:   src/components/noders/index.js 	modified:   src/components/noders/member.js 	modified:   src/components/organizers/style.less 	modified:   src/manifest.json 	modified:   src/style/index.less

--- src/components/noders/member.js
+++ src/components/noders/member.js
@@ -9,17 +9,32 @@ export default class Member extends Component {
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         style={{
-          height: '100px'
+          height: '100px',
+          stroke: '#82CD27',
+          strokeWidth: '2px',
         }}
       >
         <defs>
-          <pattern id={image} patternUnits="userSpaceOnUse" width="100" height="100">
-            <image href={image} xlinkHref={image} x="-25" width="150" height="100" />
+          <pattern
+            id={image}
+            patternUnits="userSpaceOnUse"
+            width="100"
+            height="100"
+          >
+            <image
+              href={image}
+              xlinkHref={image}
+              x="-25"
+              width="150"
+              height="100"
+            />
           </pattern>
         </defs>
-        <polygon points="50 1 95 25 95 75 50 99 5 75 5 25" fill={`url(#${image})`} />
+        <polygon
+          points="50 1 95 25 95 75 50 99 5 75 5 25"
+          fill={`url(#${image})`}
+        />
       </svg>
-
     );
   }
 }
