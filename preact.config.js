09e03b6f0dc402114ea6de1e7e7453bb361a105e
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-12-06 11:39:01 -0300

    Testing performance optimizations

---
+++ preact.config.js
@@ -0,0 +1,14 @@
+export default function (config, env, helpers) {
+  /** you can change config here **/
+
+  // const loaders = helpers.getLoaders(config);
+  // console.log('LOADERS');
+  // console.log(require('util').inspect(loaders[9], { showHidden: true, depth: null, color: true }))
+
+  // babelConfig.plugins.push('my-chosen-plugin');
+
+  // console.log('env');
+  // console.log(env)
+  // console.log('helpers');
+  // console.log(helpers)
+}
