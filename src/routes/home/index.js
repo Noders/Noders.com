4f76fe9899ba517c4e3352cf5a302bb87748a489
fforres <felipe.torressepulveda@gmail.com>
2017-12-07 00:18:16 -0300

    update

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,22 +1,45 @@
-import { h } from "preact";
-import style from "./style";
-import Hero from "../../components/hero";
-import Events from "async!../../components/events";
-import Media from "async!../../components/media";
-import Organizers from "async!../../components/organizers";
-import Noders from "async!../../components/noders";
-import CodeOfConduct from "async!../../components/coc";
-import { classJoin } from "../../helpers";
+import { h, Component } from 'preact';
+import style from './style';
+import Hero from '../../components/hero';
+import Events from 'async!../../components/events';
+import Media from 'async!../../components/media';
+import Organizers from 'async!../../components/organizers';
+import Noders from 'async!../../components/noders';
+import CodeOfConduct from 'async!../../components/coc';
+import { classJoin } from '../../helpers';
 
-const Home = () => (
-  <div class={classJoin(style.home)}>
-    <Hero />
-    <Events />
-    <Media />
-    <Organizers />
-    <Noders />
-    <CodeOfConduct />
-  </div>
-);
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      heroLoaded: false
+    };
+  }
+
+  renderTheRest = () => {
+    this.setState({
+      heroLoaded: true
+    });
+  };
+
+  render() {
+    const { heroLoaded } = this.state;
+
+    return (
+      <div class={classJoin(style.home)}>
+        <Hero onComponentDidMount={this.renderTheRest} />
+        {heroLoaded && (
+          <div>
+            <Events />
+            <Media />
+            <Organizers />
+            <Noders />
+            <CodeOfConduct />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Home;
