826743fde3ca99324de5355bda2c90a6bc2e9b44
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2018-03-08 11:43:21 -0800

    WIP - Agregando una primera idea a los hexagonos sobrepuestos (#101)
    * Adding first approach to snapping hexagons
    
    * Agregando separacion y testeando interaccion con los hexagonos
    
    * Add some spacing
    
    * update
    
    * adding clickable mobilks and removing outline
    
    * Adding cursor pointer
    
    * adding hack
    
    * cambiando organizer

--- src/components/noders/index.js
+++ src/components/noders/index.js
@@ -5,17 +5,19 @@ import { classJoin } from '../../helpers';
 
 export default class Noders extends Component {
   static triggers = ['scroll', 'resize', 'load'];
+  static scrollOffset = 150;
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       ready: false,
       scrollOffset: 150,
-      noders: []
+      noders: [],
+      hexagons: []
     };
   }
 
-  shuffleArray = (oldArray) => {
+  shuffleArray = oldArray => {
     const array = [...oldArray];
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -24,13 +26,18 @@ export default class Noders extends Component {
       array[j] = temp;
     }
     return array;
-  }
+  };
 
   componentWillMount() {
-    import('./data.json')
-      .then(data => this.setState({
+    import('./data.json').then(data =>
+      this.setState({
         noders: this.shuffleArray(data.users)
-      }));
+      })
+    );
+  }
+
+  componentWillUnmount() {
+    this.stopListening();
   }
 
   componentDidMount() {
@@ -42,30 +49,112 @@ export default class Noders extends Component {
       return;
     }
     Noders.triggers.forEach(event => {
-      window.addEventListener(event, this.checkComponent, {passive: true});
+      window.addEventListener(event, this.checkComponent, { passive: true });
     });
     this.checkComponent();
-  }
+  };
 
   stopListening = () => {
     Noders.triggers.forEach(event => {
-      window.removeEventListener(event, this.checkComponent)
+      window.removeEventListener(event, this.checkComponent);
     });
-  }
+  };
 
   checkComponent = () => {
-    const { scrollOffset } = this.state;
+    const { scrollOffset } = Noders;
     if (!this.containerRef) {
       return null;
     }
     const windowHeight = window.outerHeight;
-    const { bottom } = this.containerRef.getBoundingClientRect()
+    const { bottom } = this.containerRef.getBoundingClientRect();
     if (bottom - scrollOffset < windowHeight) {
       this.setState({
         ready: true
-      }, this.stopListening)
+      });
     }
-  }
+    this.recalculateHexagons();
+  };
+
+  recalculateHexagons = () => {
+    if (!this.hexagonsRef) {
+      return;
+    }
+    const hexagonWidth = this.getHexagonsWidth();
+    const { width } = this.hexagonsRef.getBoundingClientRect();
+    console.log('width', width, hexagonWidth);
+    let evenRow = Math.floor(width / hexagonWidth) - 1;
+
+    if (width < 833 && width > 613) {
+      evenRow = evenRow - 1;
+    }
+    const oddRow = evenRow - 1;
+
+    this.setState(
+      {
+        hexagonWidth,
+        width,
+        evenRow,
+        oddRow
+      },
+      this.renderHexagons
+    );
+  };
+
+  getHexagonsWidth = () => {
+    if (!this.hexagonsRef) {
+      return;
+    }
+    const { width } = this.hexagonsRef.getBoundingClientRect();
+    if (width > 768) {
+      return 100;
+    }
+    if (width > 425) {
+      return 80;
+    }
+    return 60;
+  };
+  renderHexagons = () => {
+    const hexagons = [];
+    const { evenRow, oddRow, noders } = this.state;
+    let isEvenRow = false;
+    let currentNode = [];
+    if (!evenRow || !oddRow) {
+      return;
+    }
+    noders.forEach((member, index) => {
+      const currentRowWidth = isEvenRow ? evenRow : oddRow;
+      if (currentNode.length === currentRowWidth) {
+        isEvenRow = !isEvenRow;
+        hexagons.push(
+          <div className={style.nodersRow}>{[...currentNode]}</div>
+        );
+        currentNode = [];
+      }
+
+      currentNode.push(
+        <Member
+          username={member.username}
+          image={member.image}
+          id={`${index}-${hexagons.length}`}
+        />
+      );
+    });
+
+    // Si hay exagonos que no se han agregado al array, agregarlos
+    if (currentNode.length) {
+      const isPreviousRowEven =
+        hexagons[hexagons.length - 1].children.length % 2 === 0;
+      const isLastRowEven = currentNode.length % 2 === 0;
+      const shouldAddFakeHexagon = isPreviousRowEven && isLastRowEven;
+      // Si la ultima fila es par (o impar) y la anterior tambien es par (o impar), agregar un padding para 'finjir' otro elemento, y que el css se encargue de hacer caer loshexagonos correctamente
+      const lastRowClasses = classJoin(
+        style.nodersRow,
+        shouldAddFakeHexagon && style.addFakeHexagon
+      );
+      hexagons.push(<div className={lastRowClasses}>{[...currentNode]}</div>);
+    }
+    this.setState({ hexagons });
+  };
 
   render() {
     const { ready } = this.state;
@@ -81,12 +170,14 @@ export default class Noders extends Component {
             <hr />
           </div>
           {ready && (
-            <div class={classJoin('level', style.flexContainer, 'section-content')}>
-              {this.state.noders.map(member => <Member username={member.username} image={member.image} />)}
+            <div
+              class={classJoin('level', style.flexContainer)}
+              ref={c => (this.hexagonsRef = c)}
+            >
+              {this.state.hexagons}
             </div>
           )}
         </div>
-
       </section>
     );
   }
--- src/components/noders/member.js
+++ src/components/noders/member.js
@@ -1,25 +1,23 @@
 import { h, Component } from 'preact';
+import style from './style';
+import { classJoin } from '../../helpers';
 
 export default class Member extends Component {
   render() {
-    const { image } = this.props;
+    const { image, username, id, onClick } = this.props;
     return (
-      <svg
-        viewBox="0 0 100 100"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        style={{
-          height: '100px'
-        }}
-      >
-        <defs>
-          <pattern id={image} patternUnits="userSpaceOnUse" width="100" height="100">
-            <image href={image} xlinkHref={image} x="-25" width="150" height="100" />
-          </pattern>
-        </defs>
-        <polygon points="50 1 95 25 95 75 50 99 5 75 5 25" fill={`url(#${image})`} />
-      </svg>
-
+      <div className={style.hexagonContainer}>
+        <div
+          id={id}
+          onClick={onClick}
+          class={classJoin(style.hexagon)}
+          style={{ backgroundImage: `url(${image})` }}
+        >
+          <div class={classJoin(style.hexTop)} />
+          <div class={classJoin(style.hexBottom)} />
+        </div>
+        <div class={classJoin(style.backgroundText)}>@{username}</div>
+      </div>
     );
   }
 }
