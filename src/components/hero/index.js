26764d8f2436e6fc6ef49f26fb2517cff28617c7
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2018-03-22 22:00:43 -0700

    fix error

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,7 +1,7 @@
 import { h, Component } from 'preact';
 import style from './style';
-import image from '../../assets/noders/noderslogo_2.png';
 import { classJoin } from '../../helpers';
+import Picture from '../picture';
 
 export default class Hero extends Component {
   constructor(props) {
@@ -18,7 +18,7 @@ export default class Hero extends Component {
   load = () => {
     this.setState({ load: true });
     this.props.onComponentDidMount();
-  }
+  };
 
   componentDidMount() {
     window.requestAnimationFrame(this.load);
@@ -35,7 +35,7 @@ export default class Hero extends Component {
           style.nodersLogo
         )}
       >
-        <img alt="Logo Noders" src={image} />
+        <Picture alt="Logo Noders" imageName="noderslogo_2" />
         <h1>¿Por qué? Porque nos gusta</h1>
         <a
           href="https://fforres.typeform.com/to/z2Rj7z"
