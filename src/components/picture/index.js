7e374f7fba33670c8deb461579c280f676be320e
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2018-03-22 21:42:49 -0700

    adding webp

--- src/components/picture/index.js
+++ src/components/picture/index.js
@@ -5,21 +5,23 @@ class Picture extends Component {
     const { imageName, alt, folder, ...props } = this.props;
     const folderRoute = folder ? `${folder}/` : '';
     return (
-      <picture {...props}>
-        <source
-          srcset={`../../assets/images/${folderRoute}${imageName}.webp`}
-          type="image/webp"
-        />
-        <source
-          srcset={`../../assets/images/${folderRoute}${imageName}.png`}
-          type="image/webp"
-        />
-        <img
-          src={`../../assets/images/${folderRoute}${imageName}.png`}
-          type="image/webp"
-          alt={alt}
-        />
-      </picture>
+      <div {...props}>
+        <picture>
+          <source
+            srcset={`../../assets/images/${folderRoute}${imageName}.webp`}
+            type="image/webp"
+          />
+          <source
+            srcset={`../../assets/images/${folderRoute}${imageName}.png`}
+            type="image/webp"
+          />
+          <img
+            src={`../../assets/images/${folderRoute}${imageName}.png`}
+            type="image/webp"
+            alt={alt}
+          />
+        </picture>
+      </div>
     );
   }
 }
