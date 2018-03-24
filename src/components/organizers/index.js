0cadf547d0c01bf9c08ec37e4c12a1fa07180fc7
Ender Bonnet <enndbo@gmail.com>
2018-03-23 21:11:23 -0300

    Cambio de Organizers a Component
    Renderizar los organizadores en conjunto, no uno por uno.

--- src/components/organizers/index.js
+++ src/components/organizers/index.js
@@ -1,41 +1,53 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import style from './style';
 import members from '../../assets/data/members';
 import { classJoin } from '../../helpers';
 
-let getOrganizerSocial = (member) => {
-  if (member.github) {
-    return {
-      label: `Perfil de Github de ${member.github}`,
-      href: `https://github.com/${member.github}`
+class Organizers extends Component {
+
+  getOrganizerSocial = (member) => {
+    if (member.github) {
+      return {
+        label: `Perfil de Github de ${member.github}`,
+        href: `https://github.com/${member.github}`
+      }
     }
-  }
-  return {
-    label: `Perfil de Twitter de ${member.twitter}`,
-    href: `https://twitter.com/${member.twitter}`
+    return {
+      label: `Perfil de Twitter de ${member.twitter}`,
+      href: `https://twitter.com/${member.twitter}`
+    }
+  };
+
+  renderOrganizers = () => {
+    return members.map((member) => {
+      const { label, href } = this.getOrganizerSocial(member);
+      return (
+        <div className={classJoin('level-item', 'has-text-centered', style.organizer)}>
+          <figure>
+            <a target="_blank" aria-label={label} href={href} rel="noopener"><img src={`../../assets/images/organizers/${member.image}`} alt={label} className={style.memberImage} /></a>
+            <p className={style.memberInfo}><a href={href}>@{member.username}</a></p>
+          </figure>
+        </div>
+      )
+    })
+  };
+
+  render() {
+    return (
+      <section className={classJoin('section', style.section)}>
+        <div className="container">
+          <a name="comunidad" />
+          <div className="section-title">
+            <h1 className="title">Organizadores</h1>
+            <hr />
+          </div>
+          <div class={classJoin('level', 'section-content', style.organizersContainer)}>
+            {this.renderOrganizers()}
+          </div>
+        </div>
+      </section>
+    )
   }
 }
-
-const Organizers = () => (
-  <section className={classJoin('section', style.section)}>
-    <div className="container">
-      <a name="comunidad" />
-      <div className="section-title">
-        <h1 className="title">Organizadores</h1>
-        <hr />
-      </div>
-      <div class={classJoin('level', 'section-content', style.organizersContainer)}>
-        {members.map((member) =>
-          (<div className={classJoin('level-item', 'has-text-centered', style.organizer)}>
-            <figure>
-              <a target="_blank" aria-label={getOrganizerSocial(member).label} href={getOrganizerSocial(member).href} rel="noopener"><img src={`../../assets/images/organizers/${member.image}`} alt={getOrganizerSocial(member).label} className={style.memberImage} /></a>
-              <p className={style.memberInfo}><a href={getOrganizerSocial(member).href}>@{member.username}</a></p>
-            </figure>
-          </div>)
-        )}
-      </div>
-    </div>
-  </section>
-);
 
 export default Organizers;
