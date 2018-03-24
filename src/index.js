b24029083a7a96e18422b5c5665de39ca53adcdb
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2018-03-24 00:44:27 -0700

    prerender

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import 'intersection-observer';
+if (typeof window !== 'undefined') {
+  require('intersection-observer');
+}
+
+/*
+  eslint-disable import/first
+*/
 import './style';
 import './assets/fonts/styles.css';
 import 'bulma/css/bulma.css';
