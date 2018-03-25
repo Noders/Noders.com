23d152044c690218ba03639783641714562e2a14
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2018-03-25 11:43:18 -0700

    do not inline (?)

--- postbuild.js
+++ postbuild.js
@@ -1,13 +1,25 @@
 const critical = require('critical');
+const { open, close } = require('fs');
+const { resolve } = require('path')
+const { promisify } = require('util');
+const openPromisified = promisify(open);
+const closePromisified = promisify(close);
+
+const filename = 'critical.css'
+const touch = filename => {
+  const route = resolve('build', filename)
+  console.log(route)
+  return openPromisified(route, 'w').then(closePromisified);
+}
 
 const start = async () => {
   try {
+    await touch(filename)
     await critical.generate({
-      inline: true,
       minify: true,
       base: 'build',
       src: 'index.html',
-      dest: 'index.html',
+      dest: filename,
       dimensions: [{
         height: 740,
         width: 420
