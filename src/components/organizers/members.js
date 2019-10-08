945b299ee37dd8a16256829ab281cc92ed2d8434
Ender Bonnet <enbonnet@gmail.com>
2019-10-08 09:37:31 -0300

    update organizers (#134)
    Committer: Ender Bonnet <enbonnet@gmail.com>

--- src/components/organizers/members.js
+++ src/components/organizers/members.js
@@ -1,43 +1,13 @@
 const members = [
   {
-    username: 'fforres',
-    image: 'fforres.jpg',
-    twitter: 'fforres'
-  },
-  {
-    username: 'DMaster69',
-    image: 'dmaster.jpg',
-    twitter: 'DMaster69'
-  },
-  {
-    username: 'cristofer-dev',
-    image: 'cristofer.jpg',
-    twitter: 'cristofer_dev'
-  },
-  {
     username: 'L0rdKras',
     image: 'lordkras.jpg',
     twitter: 'L0rdKras'
   },
   {
-    username: 'luchosrock',
-    image: 'luchosrock.jpg',
-    twitter: 'luismairiu'
-  },
-  {
     username: 'enBonnet',
     image: 'bonnet.jpg',
     twitter: 'enBonnet'
-  },
-  {
-    username: 'carolina',
-    image: 'carolina.jpg',
-    github: 'carolina-pinzon'
-  },
-  {
-    username: 'max',
-    image: 'creedarky.jpg',
-    twitter: 'creedarky'
   }
 ];
 
