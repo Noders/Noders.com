fc4b353add0e18235e7b51589777ce5ab9a91074
Luis Porras <lporras16@gmail.com>
2019-10-19 16:17:39 -0300

    Rename to Noders FM (#136)
    * Update index.js
    
    Update noderscast to Noders.fm
    
    * update to https

--- src/components/media/index.js
+++ src/components/media/index.js
@@ -32,7 +32,7 @@ const Media = () => (
         <div className="level-item has-text-centered">
           <figure>
             <a
-              href="https://anchor.fm/noderscast"
+              href="https://www.noders.fm"
               target="_blank"
               rel="noopener"
               className={style}
